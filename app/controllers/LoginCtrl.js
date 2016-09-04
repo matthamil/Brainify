c308ef44fd2ead6cc8905462f165e846840681d7
Matt Hamil <j.matthew.hamil@gmail.com>
2016-09-03 19:42:33 -0500

    Added comments to LoginCtrl

--- app/controllers/LoginCtrl.js
+++ app/controllers/LoginCtrl.js
@@ -1,16 +1,25 @@
 'use strict';
 
 app.controller('LoginCtrl', function($scope, Spotify) {
+  /**
+   * Creates a popup window for a user to login and saves the auth token to local storage
+   */
   $scope.login = () => {
     Spotify.login();
   };
 
+  /**
+   * Logs the current user information to the console
+   */
   $scope.userTest = () => {
     Spotify.getCurrentUser().then((data) => {
       console.log(data);
     });
   };
 
+  /**
+   * Logs the audio features for the test song
+   */
   $scope.audioFeatureTest = () => {
     Spotify.getTrackAudioFeatures('0eGsygTp906u18L0Oimnem').then(function (data) {
       console.log(data);
