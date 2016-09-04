dcd9f4664d64cc1bcce4d5b9d0c1127685c0c84a
Matt Hamil <j.matthew.hamil@gmail.com>
2016-09-03 20:03:38 -0500

    Added link to angular-spotify in comment

--- app/controllers/LoginCtrl.js
+++ app/controllers/LoginCtrl.js
@@ -4,6 +4,7 @@ app.controller('LoginCtrl', function($scope, Spotify) {
   /**
    * Creates a popup window for a user to login and saves the auth token to local storage.
    * This function relies on app/SpotifyConfig.js which is not included in the project repo.
+   * Refer to https://github.com/eddiemoore/angular-spotify#usage to configure SpotifyProvider.
    */
   $scope.login = () => {
     Spotify.login();
