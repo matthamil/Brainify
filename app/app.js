da50702648024e3ce9f89cc1dd639ee4913e8987
Matt Hamil <j.matthew.hamil@gmail.com>
2016-09-03 22:22:48 -0500

    Updated routes

--- app/app.js
+++ app/app.js
@@ -4,9 +4,9 @@ let app = angular.module('Brainify', ['ngRoute', 'spotify']);
 
 app.config(function($routeProvider) {
   $routeProvider
-    .when('/landing-page', {
+    .when('/', {
       templateUrl: 'partials/landing-page.html',
       controller: 'LoginCtrl'
     })
-    .otherwise('/landing-page');
+    .otherwise('/');
 });
