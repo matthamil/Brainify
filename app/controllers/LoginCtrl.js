2ea8d20b50909797eea1a3b310f6b8c8f5c3abd9
Matt Hamil <j.matthew.hamil@gmail.com>
2016-09-04 01:35:53 -0500

    Removed  as a dependency for LoginCtrl

--- app/controllers/LoginCtrl.js
+++ app/controllers/LoginCtrl.js
@@ -4,7 +4,7 @@
  * Login Controller
  * Dependencies: $scope, $location, Spotify (from angular-spotify)
  */
-app.controller('LoginCtrl', function($scope, $location, $routeParams, Spotify) {
+app.controller('LoginCtrl', function($scope, $location, Spotify) {
   /**
    * Creates a popup window for a user to login and saves the auth token to local storage.
    * This function relies on app/SpotifyConfig.js which is not included in the project repo.
