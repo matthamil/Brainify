201c150ff3b319d057ed53bbcf8041df2ff5b296
Matt Hamil <j.matthew.hamil@gmail.com>
2016-09-11 21:53:24 -0500

    WIP: Updating how the neural network is trained

--- app/factories/FirebaseFactory.js
+++ app/factories/FirebaseFactory.js
@@ -1,10 +1,28 @@
 'use strict';
 
 function FirebaseFactory(($q, $http) => {
-  
+
+  function getSongFeaturesFromGenre(genre) {
+    return $q((resolve, reject) => {
+      $http.get(`https://brainify-ddc05.firebaseio.com/-KRQvqIBCFfxEsFaovny/${genre}.json`)
+        .then((data) => {
+          resolve(data);
+        }, (rejectData) => {
+          reject(rejectData);
+        })
+    });
+  }
+
+  function getNegativeGenresSongFeatures(genreList) {
+    return $q.all(
+      return genreList.map((genre) => {
+        return getSongFeaturesFromGenre(genre);
+      })
+    );
+  }
 
   return {
-
+    getSongFeaturesFromGenre
   };
 });
 
